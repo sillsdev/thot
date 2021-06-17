f870fef8221f23c615058ba01693af0568388a26
Damien Daspit <damien_daspit@sil.org>
2021-06-17 23:10:31 +0700

    Refactor OrderedVector so all methods are inline

--- src/nlp_common/OrderedVector.h
+++ src/nlp_common/OrderedVector.h
@@ -39,31 +39,118 @@ public:
   using iterator = typename std::vector<std::pair<KEY, DATA>>::iterator;
   using const_iterator = typename std::vector<std::pair<KEY, DATA>>::const_iterator;
 
-  OrderedVector();
-  OrderedVector(const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov);
-  // Copy-constructor
-  OrderedVector<KEY, DATA, KEY_ORDER_REL>& operator=(const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov);
-  DATA* push(const KEY& k, const DATA& d);
-  DATA* insert(const KEY& k, const DATA& d);
-  void pop();
-  const std::pair<KEY, DATA>& top() const;
-  DATA* findPtr(const KEY& k);
-  iterator find(const KEY& k);
-  DATA& operator[](const KEY& k);
-  const std::pair<KEY, DATA>& getAt(size_t index);
-  bool empty() const;
-  size_t size() const;
-  void clear();
+  OrderedVector()
+  {
+  }
 
-  // Constant iterator functions for the OrderedVector class
-  const_iterator begin() const;
-  const_iterator end() const;
+  OrderedVector(const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov) : data(ov.data)
+  {
+  }
 
-  // Iterator functions for the OrderedVector class
-  iterator begin();
-  iterator end();
+  OrderedVector<KEY, DATA, KEY_ORDER_REL>& operator=(const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov)
+  {
+    if (&ov != this)
+      data = ov.data;
+    return *this;
+  }
 
-  ~OrderedVector();
+  DATA* push(const KEY& k, const DATA& d)
+  {
+    iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
+    if (iter == data.end() || compare(k, *iter))
+    {
+      iter = data.insert(iter, std::make_pair(k, d));
+      return &(iter->second);
+    }
+
+    iter->second = d;
+    return &(iter->second);
+  }
+
+  DATA* insert(const KEY& k, const DATA& d)
+  {
+    return push(k, d);
+  }
+
+  void pop()
+  {
+    data.pop_back();
+  }
+
+  const std::pair<KEY, DATA>& top() const
+  {
+    return data[size() - 1];
+  }
+
+  DATA* findPtr(const KEY& k)
+  {
+    iterator iter = find(k);
+    if (iter == data.end())
+      return nullptr;
+    return &(iter->second);
+  }
+
+  iterator find(const KEY& k)
+  {
+    iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
+    return iter != data.end() && !compare(k, *iter) ? iter : data.end();
+  }
+
+  DATA& operator[](const KEY& k)
+  {
+    iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
+    if (iter == data.end() || compare(k, *iter))
+    {
+      DATA d = DATA();
+      iter = data.insert(iter, std::make_pair(k, d));
+    }
+    return iter->second;
+  }
+
+  const std::pair<KEY, DATA>& getAt(size_t index)
+  {
+    return data[index];
+  }
+
+  bool empty() const
+  {
+    return data.empty();
+  }
+
+  size_t size() const
+  {
+    return data.size();
+  }
+
+  void clear()
+  {
+    return data.clear();
+  }
+
+  const_iterator begin() const
+  {
+    return data.begin();
+  }
+
+  const_iterator end() const
+  {
+    return data.end();
+  }
+
+  iterator begin()
+  {
+    return data.begin();
+  }
+
+  iterator end()
+  {
+    return data.end();
+  }
+
+  ~OrderedVector()
+  {
+    data.clear();
+  }
 
 protected:
   class PairCompare
@@ -95,139 +182,5 @@ protected:
   std::vector<std::pair<KEY, DATA>> data;
   PairCompare compare;
 };
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-OrderedVector<KEY, DATA, KEY_ORDER_REL>::OrderedVector()
-{
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-OrderedVector<KEY, DATA, KEY_ORDER_REL>::OrderedVector(const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov)
-    : data(ov.data)
-{
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-OrderedVector<KEY, DATA, KEY_ORDER_REL>& OrderedVector<KEY, DATA, KEY_ORDER_REL>::operator=(
-    const OrderedVector<KEY, DATA, KEY_ORDER_REL>& ov)
-{
-  if (&ov != this)
-    data = ov.data;
-  return *this;
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-DATA* OrderedVector<KEY, DATA, KEY_ORDER_REL>::push(const KEY& k, const DATA& d)
-{
-  iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
-  if (iter == data.end() || compare(k, *iter))
-  {
-    iter = data.insert(iter, std::make_pair(k, d));
-    return &(iter->second);
-  }
-
-  iter->second = d;
-  return &(iter->second);
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-DATA* OrderedVector<KEY, DATA, KEY_ORDER_REL>::insert(const KEY& k, const DATA& d)
-{
-  return push(k, d);
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-void OrderedVector<KEY, DATA, KEY_ORDER_REL>::pop()
-{
-  data.pop_back();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-const std::pair<KEY, DATA>& OrderedVector<KEY, DATA, KEY_ORDER_REL>::top() const
-{
-  return data[size() - 1];
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-DATA* OrderedVector<KEY, DATA, KEY_ORDER_REL>::findPtr(const KEY& k)
-{
-  iterator iter = find(k);
-  if (iter == data.end())
-    return nullptr;
-  return &(iter->second);
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-typename OrderedVector<KEY, DATA, KEY_ORDER_REL>::iterator OrderedVector<KEY, DATA, KEY_ORDER_REL>::find(const KEY& k)
-{
-  iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
-  return iter != data.end() && !compare(k, *iter) ? iter : data.end();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-DATA& OrderedVector<KEY, DATA, KEY_ORDER_REL>::operator[](const KEY& k)
-{
-  iterator iter = std::lower_bound(data.begin(), data.end(), k, compare);
-  if (iter == data.end() || compare(k, *iter))
-  {
-    DATA d = DATA();
-    iter = data.insert(iter, std::make_pair(k, d));
-  }
-  return iter->second;
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-const std::pair<KEY, DATA>& OrderedVector<KEY, DATA, KEY_ORDER_REL>::getAt(size_t index)
-{
-  return data[index];
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-bool OrderedVector<KEY, DATA, KEY_ORDER_REL>::empty() const
-{
-  return this->size() == 0;
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-size_t OrderedVector<KEY, DATA, KEY_ORDER_REL>::size() const
-{
-  return data.size();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-void OrderedVector<KEY, DATA, KEY_ORDER_REL>::clear()
-{
-  data.clear();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-typename OrderedVector<KEY, DATA, KEY_ORDER_REL>::const_iterator OrderedVector<KEY, DATA, KEY_ORDER_REL>::begin() const
-{
-  return data.begin();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-typename OrderedVector<KEY, DATA, KEY_ORDER_REL>::const_iterator OrderedVector<KEY, DATA, KEY_ORDER_REL>::end() const
-{
-  return data.end();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-typename OrderedVector<KEY, DATA, KEY_ORDER_REL>::iterator OrderedVector<KEY, DATA, KEY_ORDER_REL>::begin()
-{
-  return data.begin();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-typename OrderedVector<KEY, DATA, KEY_ORDER_REL>::iterator OrderedVector<KEY, DATA, KEY_ORDER_REL>::end()
-{
-  return data.end();
-}
-
-template <class KEY, class DATA, class KEY_ORDER_REL>
-OrderedVector<KEY, DATA, KEY_ORDER_REL>::~OrderedVector()
-{
-  clear();
-}
 
 #endif
