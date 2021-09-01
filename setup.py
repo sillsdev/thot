7baf67691f54ae2824ec8e412c703154d475ef1a
Damien Daspit <damien_daspit@sil.org>
2021-09-01 13:00:45 +0700

    Update version to 3.3.3

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="sil-thot",
-    version="3.3.2",
+    version="3.3.3",
     author="SIL International",
     maintainer="Damien Daspit",
     maintainer_email="damien_daspit@sil.org",
